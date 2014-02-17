bf831c311f9452934734bdc33b70d952b3f9fbb6
Josh <buker.joshua@gmail.com>
2014-02-16 19:36:31 -0800

    Updated aeoninput to being a class instead of global functions. (Each context can create a new InputHandler and use it separately from each other.)

--- source/aeoninput.cpp
+++ source/aeoninput.cpp
@@ -11,22 +11,20 @@ using namespace std;
 
 namespace aeon
 {
-    map<string,int> knownInputs;
-
-    void getInputSettings(Config * settings)
+    void InputHandler::setInputSettings(Config * settings)
     {
-        setInput("LMB",initKeyPair(settings, "input", "LMB", GLFW_MOUSE_BUTTON_LEFT));
-        setInput("RMB",initKeyPair(settings, "input", "RMB", GLFW_MOUSE_BUTTON_RIGHT));
-        setInput("CAMERA_FORWARD",initKeyPair(settings, "input", "CAMERA_FORWARD", GLFW_KEY_W));
-        setInput("CAMERA_BACK",initKeyPair(settings, "input", "CAMERA_BACK", GLFW_KEY_S));
-        setInput("CAMERA_LEFT",initKeyPair(settings, "input", "CAMERA_LEFT", GLFW_KEY_A));
-        setInput("CAMERA_RIGHT",initKeyPair(settings, "input", "CAMERA_RIGHT", GLFW_KEY_D));
-        setInput("CAMERA_UP",initKeyPair(settings, "input", "CAMERA_UP", GLFW_KEY_Q));
-        setInput("CAMERA_DOWN",initKeyPair(settings, "input", "CAMERA_DOWN", GLFW_KEY_Z));
-        setInput("CLOSE",initKeyPair(settings, "input", "CLOSE", GLFW_KEY_ESCAPE));
-        setInput("DERP",initKeyPair(settings, "input", "DERP", GLFW_KEY_F12));
+        setInput("LMB",             initKeyPair(settings, "input", "LMB",               GLFW_MOUSE_BUTTON_LEFT));
+        setInput("RMB",             initKeyPair(settings, "input", "RMB",               GLFW_MOUSE_BUTTON_RIGHT));
+        setInput("CAMERA_FORWARD",  initKeyPair(settings, "input", "CAMERA_FORWARD",    GLFW_KEY_W));
+        setInput("CAMERA_BACK",     initKeyPair(settings, "input", "CAMERA_BACK",       GLFW_KEY_S));
+        setInput("CAMERA_LEFT",     initKeyPair(settings, "input", "CAMERA_LEFT",       GLFW_KEY_A));
+        setInput("CAMERA_RIGHT",    initKeyPair(settings, "input", "CAMERA_RIGHT",      GLFW_KEY_D));
+        setInput("CAMERA_UP",       initKeyPair(settings, "input", "CAMERA_UP",         GLFW_KEY_Q));
+        setInput("CAMERA_DOWN",     initKeyPair(settings, "input", "CAMERA_DOWN",       GLFW_KEY_Z));
+        setInput("CLOSE",           initKeyPair(settings, "input", "CLOSE",             GLFW_KEY_ESCAPE));
+        setInput("DERP",            initKeyPair(settings, "input", "DERP",              GLFW_KEY_F12));
     }
-    void setInput(string key,int value)
+    void InputHandler::setInput(string key,int value)
     {
         if(inputExists(key))
         {
@@ -35,7 +33,7 @@ namespace aeon
         knownInputs.insert( pair<string,int>(key,value));
     }
 
-    bool inputExists(string input)
+    bool InputHandler::inputExists(string input)
     {
         if(knownInputs.find(input)!=knownInputs.end())
         {
@@ -46,36 +44,39 @@ namespace aeon
             return false;
         }
     }
-    int getInput(string input)
+    int InputHandler::getInput(string input)
     {
         if(inputExists(input))
         {
             return knownInputs.at(input);
         }
-        return 0;
+        else
+        {
+            throw invalid_argument("Input not found.");
+        }
     }
 
-    bool isMouseDown(string button)
+    bool InputHandler::isMouseDown(string button)
     {
         if(inputExists(button))
         {
-            return glfwGetMouseButton(getMainWindowHandle(), getInput(button));
+            return glfwGetMouseButton(aWindowHandle, getInput(button));
         }
         else
         {
-            return false;
+            throw invalid_argument("Input not found.");
         }
     }
 
-    bool isKeyDown(string key)
+    bool InputHandler::isKeyDown(string key)
     {
         if(inputExists(key))
         {
-            return glfwGetKey(getMainWindowHandle(), getInput(key));
+            return glfwGetKey(aWindowHandle, getInput(key));
         }
         else
         {
-            return false;
+            throw invalid_argument("Input not found.");
         }
     }
 }
--- source/aeoninput.hpp
+++ source/aeoninput.hpp
@@ -1,19 +1,33 @@
 #ifndef _AEONINPUT_
 #define _AEONINPUT_
 
+#include <GLFW/glfw3.h>
+
 #include <string>
 
 namespace aeon
 {
     class Config;
 
-    void getInputSettings(Config * settings);
-    void setInput(std::string key,int value);
-    bool inputExists(std::string input);
-    int getInput(std::string input);
+    class InputHandler
+    {
+    public:
+        InputHandler();
+        ~InputHandler();
 
-    bool isMouseDown(std::string button);
-    bool isKeyDown(std::string key);
+        void setWindowHandle(GLFWwindow * windowHandle);
+        virtual void setInputSettings(Config * settings);
+
+        void setInput(std::string key,int value);
+        int getInput(std::string input);
+        bool inputExists(std::string input);
+
+        bool isMouseDown(std::string button);
+        bool isKeyDown(std::string key);
+    private:
+        map<string,int> knownInputs;
+        GLFWwindow * aWindowHandle;
+    };
 }
 
 #endif // _AEONINPUT_
