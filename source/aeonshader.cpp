6dd4fb2c6ef91f32c4d3d28c29c95d39543a84d5
Josh <buker.joshua@gmail.com>
2014-02-23 17:07:54 -0800

    Fixes #5
    Full shader support implemented. (Vertex/Fragment program linking that is...)

--- source/aeonshader.cpp
+++ source/aeonshader.cpp
@@ -121,4 +121,130 @@ namespace aeon
 
         return programID;
     }
+
+    GLuint initProgram(string vertexFile, string fragmentFile, string defaultVertex, string defaultFragment)
+    {
+        // Create shaders
+        GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+        GLuint fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+        string tempV = getAeonDir()+vertexFile;
+        string tempF = getAeonDir()+fragmentFile;
+
+        string vertexCode;
+        string fragmentCode;
+
+        // Read vertex shader from file
+        ifstream vertexShaderStream(tempV.c_str(), std::ios::in);
+        if(vertexShaderStream.is_open())
+        {
+            string vLine = "";
+            while(getline(vertexShaderStream, vLine))
+                vertexCode += "\n" + vLine;
+            vertexShaderStream.close();
+        }
+        else
+        {
+            log("Failed to open vertex shader at: \""+tempV+"\"", AEON_WARNING);
+            log("Using default vertex shader.", AEON_WARNING);
+            vertexCode = defaultVertex;
+            ofstream vertexShaderStreamO(tempV.c_str(), std::ios::out);
+            if(vertexShaderStreamO.is_open())
+            {
+                vertexShaderStreamO << vertexCode;
+                vertexShaderStreamO.close();
+            }
+            else
+            {
+                log("Failed to write vertex shader at: \""+tempV+"\"", AEON_ERROR);
+            }
+        }
+
+        // Read fragment shader from file
+        ifstream fragmentShaderStream(tempF.c_str(), std::ios::in);
+        if(fragmentShaderStream.is_open())
+        {
+            string fLine = "";
+            while(getline(fragmentShaderStream, fLine))
+                fragmentCode += "\n" + fLine;
+            fragmentShaderStream.close();
+        }
+        else
+        {
+            log("Failed to open fragment shader at: \""+tempF+"\"", AEON_WARNING);
+            log("Using default fragment shader.", AEON_WARNING);
+            fragmentCode = defaultFragment;
+            ofstream fragmentShaderStreamO(tempF.c_str(), std::ios::out);
+            if(fragmentShaderStreamO.is_open())
+            {
+                fragmentShaderStreamO << fragmentCode;
+                fragmentShaderStreamO.close();
+            }
+            else
+            {
+                log("Failed to write fragment shader at: \""+tempF+"\"", AEON_ERROR);
+            }
+        }
+
+        GLint result = GL_FALSE;
+        int infoLogLength;
+
+        // Compile Vertex Shader
+        log("Compiling vertex shader: \""+vertexFile+"\"", AEON_INFO);
+        char const * vertexSourcePointer = vertexCode.c_str();
+        glShaderSource(vertexShaderID, 1, &vertexSourcePointer, NULL);
+        glCompileShader(vertexShaderID);
+
+        // Check Vertex Shader
+        glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, &result);
+        glGetShaderiv(vertexShaderID, GL_INFO_LOG_LENGTH, &infoLogLength);
+        if ( infoLogLength > 0 )
+        {
+            std::vector<char> vertexShaderErrorMessage(infoLogLength+1);
+            glGetShaderInfoLog(vertexShaderID, infoLogLength, NULL, &vertexShaderErrorMessage[0]);
+            if(vertexShaderErrorMessage.size() > 2)
+                log(vertexShaderErrorMessage, AEON_ERROR);
+        }
+
+        // Compile Fragment Shader
+        log("Compiling fragment shader: \""+fragmentFile+"\"", AEON_INFO);
+        char const * fragmentSourcePointer = fragmentCode.c_str();
+        glShaderSource(fragmentShaderID, 1, &fragmentSourcePointer, NULL);
+        glCompileShader(fragmentShaderID);
+
+        // Check Vertex Shader
+        glGetShaderiv(fragmentShaderID, GL_COMPILE_STATUS, &result);
+        glGetShaderiv(fragmentShaderID, GL_INFO_LOG_LENGTH, &infoLogLength);
+        if ( infoLogLength > 0 )
+        {
+            std::vector<char> fragmentShaderErrorMessage(infoLogLength+1);
+            glGetShaderInfoLog(fragmentShaderID, infoLogLength, NULL, &fragmentShaderErrorMessage[0]);
+            if(fragmentShaderErrorMessage.size() > 2)
+                log(fragmentShaderErrorMessage, AEON_ERROR);
+        }
+
+        // Link program
+        log("Linking program.", AEON_INFO);
+        GLuint programID = glCreateProgram();
+        glAttachShader(programID, vertexShaderID);
+        glAttachShader(programID, fragmentShaderID);
+        glLinkProgram(programID);
+
+        // Check the program
+        glGetProgramiv(programID, GL_LINK_STATUS, &result);
+        glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &infoLogLength);
+        if ( infoLogLength > 0 )
+        {
+            std::vector<char> programErrorMessage(infoLogLength+1);
+            glGetProgramInfoLog(programID, infoLogLength, NULL, &programErrorMessage[0]);
+            if(programErrorMessage.size() > 2)
+                log(programErrorMessage, AEON_ERROR);
+        }
+
+        // Clean up
+        glDeleteShader(vertexShaderID);
+        glDeleteShader(fragmentShaderID);
+
+        return programID;
+    }
 }
--- source/aeonshader.hpp
+++ source/aeonshader.hpp
@@ -9,10 +9,10 @@ namespace aeon
 {
     GLuint initProgram(std::string vertexFile, std::string fragmentFile);
 
-    /*GLuint initProgram(std::string vertexFile,          // vertex file (just filename/type)
+    GLuint initProgram(std::string vertexFile,          // vertex file (just filename/type)
                        std::string fragmentFile,        // i.e. "test.frag" would resolve to "%aeondir%/test.frag"
                        std::string defaultVertex,
-                       std::string defaultFragment);*/
+                       std::string defaultFragment);
 }
 
 #endif // _AEONSHADER_
